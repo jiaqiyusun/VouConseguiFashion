d5e1c7e98e3a4e4354fc8643242c8646c62a7022
jiaqiyusun <jiaqi.yu.sun@gmail.com>
2021-07-04 01:47:49 +0100

    main

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_16_163921) do
+ActiveRecord::Schema.define(version: 2021_05_16_144337) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -64,9 +64,9 @@ ActiveRecord::Schema.define(version: 2021_05_16_163921) do
 
   create_table "product_images", force: :cascade do |t|
     t.integer "product_id", null: false
+    t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "image"
     t.index ["product_id"], name: "index_product_images_on_product_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
-# Category.create!([
- # {title: "MULHER", weight: 0, products_counter: 0, ancestry: nil},
-  #{title: "CRIANÇA", weight: 1, products_counter: 0, ancestry: nil},
-  #{title: "VESTIDOS  DE \"A\"", weight: 0, products_counter: 0, ancestry: "1"},
-  #{title: "VESTIDOS DE RETO", weight: 0, products_counter: 0, ancestry: "1"},
-  #{title: "MENINA | 4- 16 ANO", weight: 0, products_counter: 0, ancestry: "2"},
-  #{title: "MENINA | 2- 12 ANOS", weight: 0, products_counter: 0, ancestry: "2"},
-  #{title: "MENINO | 2- 12 ANOS", weight: 0, products_counter: 0, ancestry: "2"},
-  #{title: "MENINO | 4- 16 ANO", weight: 0, products_counter: 0, ancestry: "2"},
-  #{title: "VESTIDOS DE PRINCESA", weight: 0, products_counter: 0, ancestry: "1"},
-  #{title: "VESTIDOS DE IMPÉRIO", weight: 0, products_counter: 0, ancestry: "1"},
-  #{title: "VESTIDO DE SEREIA", weight: 0, products_counter: 0, ancestry: "1"},
-  #{title: "VESTIDOS CURTOS", weight: 0, products_counter: 0, ancestry: "1"},
-  #{title: "VESTIDOS DE MULLET", weight: 0, products_counter: 0, ancestry: "1"},
-  #{title: "FATOS", weight: 0, products_counter: 0, ancestry: "1"}
-#])
+Category.create!([
+  {title: "MULHER", weight: 0, products_counter: 0, ancestry: nil},
+  {title: "CRIANÇA", weight: 1, products_counter: 0, ancestry: nil},
+  {title: "VESTIDOS  DE \"A\"", weight: 0, products_counter: 0, ancestry: "1"},
+  {title: "VESTIDOS DE RETO", weight: 0, products_counter: 0, ancestry: "1"},
+  {title: "MENINA | 4- 16 ANO", weight: 0, products_counter: 0, ancestry: "2"},
+  {title: "MENINA | 2- 12 ANOS", weight: 0, products_counter: 0, ancestry: "2"},
+  {title: "MENINO | 2- 12 ANOS", weight: 0, products_counter: 0, ancestry: "2"},
+  {title: "MENINO | 4- 16 ANO", weight: 0, products_counter: 0, ancestry: "2"},
+  {title: "VESTIDOS DE PRINCESA", weight: 0, products_counter: 0, ancestry: "1"},
+  {title: "VESTIDOS DE IMPÉRIO", weight: 0, products_counter: 0, ancestry: "1"},
+  {title: "VESTIDO DE SEREIA", weight: 0, products_counter: 0, ancestry: "1"},
+  {title: "VESTIDOS CURTOS", weight: 0, products_counter: 0, ancestry: "1"},
+  {title: "VESTIDOS DE MULLET", weight: 0, products_counter: 0, ancestry: "1"},
+  {title: "FATOS", weight: 0, products_counter: 0, ancestry: "1"}
+])
 
 
 Size.create!([
@@ -28,17 +28,18 @@ Size.create!([
 ])
 
 Color.create!([
-  {name: "AMARELOS",codigo: "yellow"},
-  {name: "AZUIS",codigo: "blue"},
-  {name: "BRANCOS",codigo: "white"},
-  {name: "CASTANHOS",codigo: "brown"},
-  {name: "CINZENTOS",codigo: "grey"},
-  {name: "LARANJA",codigo: "orange"},
-  {name: "METALIZADOS",codigo: "silver"},
-  {name: "OUTROS",codigo: "white"},
-  {name: "PRETOS",codigo: "black"},
-  {name: "ROXOS",codigo: "purple"},
-  {name: "ROSA",codigo: "pink"},
-  {name: "VERDES",codigo: "green-coral"},
-  {name: "VERMELHOS",codigo: "red"}
+  {name: "黄色",codigo: "yellow"},
+  {name: "蓝色",codigo: "blue"},
+  {name: "白色",codigo: "white"},
+  {name: "咖啡色",codigo: "brown"},
+  {name: "灰色",codigo: "grey"},
+  {name: "橘色",codigo: "orange"},
+  {name: "金属色",codigo: "silver"},
+  {name: "其他",codigo: "white"},
+  {name: "黑色",codigo: "black"},
+  {name: "紫色",codigo: "purple"},
+  {name: "粉红色",codigo: "pink"},
+  {name: "绿色",codigo: "green-coral"},
+  {name: "红色",codigo: "red"}
 ])
+
